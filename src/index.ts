7099fb3a873fed36ce1c7f16a5416fae78180f17
Gledson Lucas <gledsonlucas111@gmail.com>
2022-06-23 10:28:35 -0300

    deploy

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use("/bike", bikeRouter);
 
-const server = app.listen(process.env.DB_PORT || 3004, () => {
+const server = app.listen(process.env.DB_PORT || 3003, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Server is running in http://localhost:${address.port}`);
